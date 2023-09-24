61c33d77c39a1163510ead0e9de1861e46227946
jcollins-01 <collinsjazmin.21@gmail.com>
2023-09-24 16:29:43 -0400

    start of script for grabbing objects with 3p body

---
+++ Assets/ThirdPersonGrab.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ThirdPersonGrab : MonoBehaviour
+{
+    public GameObject thirdPersonBodyOne;
+    public GameObject thirdPersonBodyTwo;
+    public GameObject bodyOneRightHand;
+    public GameObject bodyTwoRightHand;
+    private GameObject grabbable;
+
+    private InputDevice rightXRController;
+    private InputDevice leftXRController;
+    private bool rightControllerGrabbed = false;
+    private bool leftControllerGrabbed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void getControllers()
+    {
+        if (!rightControllerGrabbed || !leftControllerGrabbed)
+        {
+            // Makes a list for input devices + fills it with devices that match the characteristics we give in the Unity editor
+            // Narrows devices list using characteristics to just the controller we want to use
+            List<InputDevice> devices = new List<InputDevice>();
+
+            InputDeviceCharacteristics rightController = InputDeviceCharacteristics.HeldInHand & InputDeviceCharacteristics.Right;
+            InputDevices.GetDevicesWithCharacteristics(rightController, devices);
+
+            InputDeviceCharacteristics leftController = InputDeviceCharacteristics.HeldInHand & InputDeviceCharacteristics.Left;
+            InputDevices.GetDevicesWithCharacteristics(leftController, devices);
+
+            if (!rightControllerGrabbed)
+                rightXRController = devices[2]; //attached to right controller
+            if (!leftControllerGrabbed)
+                leftXRController = devices[1]; // attached to left controller
+
+            if (devices[2] != null) // rightXRController
+                rightControllerGrabbed = true;
+
+            if (devices[1] != null) // leftXRController
+                leftControllerGrabbed = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getControllers();
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //Debug.Log("Something collided with the grabbable object");
+
+        // If the grabbable collided with the first third-person body, uses right hand controller
+        // Layer 6 is Interactables
+        if (hit.transform.gameObject.layer == 6)
+        {
+            grabbable = hit.gameObject;
+            if (rightXRController.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+            {
+                if (gripValue > 0.1f)
+                {
+                    Debug.Log("Attempting grab with third person body one");
+                    GrabObject(thirdPersonBodyOne);
+                }
+            }
+        }
+
+        // If the grabbable collided with the second third-person body, uses left hand controller
+        if (hit.transform.gameObject.layer == 6)
+        {
+            grabbable = hit.gameObject;
+            if (leftXRController.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+            {
+                if (gripValue > 0.1f)
+                {
+                    Debug.Log("Attempting grab with third person body two");
+                    GrabObject(thirdPersonBodyTwo);
+                }
+            }
+        }
+    }
+
+    public void GrabObject(GameObject grabbingBody)
+    {
+        Debug.Log("Trying to grab with " + grabbingBody);
+        //grabbable.transform.SetParent(grabbingBody.transform);
+    }
+}
