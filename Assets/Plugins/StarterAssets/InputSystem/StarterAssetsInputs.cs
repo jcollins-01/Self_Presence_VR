1648d14809534157cb007137e00f29f041153a2a
jcollins-01 <collinsjazmin.21@gmail.com>
2023-09-24 15:35:25 -0400

    working 3p controller, added two 3p, two bodies scene

--- Assets/Plugins/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/Plugins/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -7,6 +7,10 @@ namespace StarterAssets
 {
 	public class StarterAssetsInputs : MonoBehaviour
 	{
+		public InputActionProperty moveAction;
+		public InputActionProperty jumpAction;
+		public InputActionProperty sprintAction;
+
 		[Header("Character Input Values")]
 		public Vector2 move;
 		public Vector2 look;
@@ -20,8 +24,15 @@ namespace StarterAssets
 		public bool cursorLocked = true;
 		public bool cursorInputForLook = true;
 
+        private void Start()
+        {
+			moveAction.action.performed += x => MoveInput(x.ReadValue<Vector2>());
+			jumpAction.action.performed += x => JumpInput(x.action.IsPressed());
+			sprintAction.action.performed += x => SprintInput(x.action.IsPressed());
+		}
+
 #if ENABLE_INPUT_SYSTEM
-		public void OnMove(InputValue value)
+        public void OnMove(InputValue value)
 		{
 			MoveInput(value.Get<Vector2>());
 		}
@@ -49,7 +60,7 @@ namespace StarterAssets
 		public void MoveInput(Vector2 newMoveDirection)
 		{
 			move = newMoveDirection;
-		} 
+		}
 
 		public void LookInput(Vector2 newLookDirection)
 		{
